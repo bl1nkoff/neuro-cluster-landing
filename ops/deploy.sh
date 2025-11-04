d9a27c6e125bdd8175e19b831299eb3b2cb666a7
bl1nkoff <bl1nkoff.dev@gmail.com>
2025-11-04 20:47:37 +0300

    ops

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -2,13 +2,13 @@
 set -e
 
 # === Проверяем аргументы ===
-NEED_TO_BUILD=false
+#NEED_TO_BUILD=false
 NEED_TO_DEPLOY_CREDENTIALS=false
 
 for arg in "$@"; do
-  if [[ "$arg" == "--build" || "$arg" == "-b" ]]; then
-    NEED_TO_BUILD=true
-  fi
+#  if [[ "$arg" == "--build" || "$arg" == "-b" ]]; then
+#    NEED_TO_BUILD=true
+#  fi
 
   if [[ "$arg" == "--credentials" || "$arg" == "-c" ]]; then
     NEED_TO_DEPLOY_CREDENTIALS=true
@@ -17,26 +17,11 @@ done
 
 
 # === Билд ===
-if [ "$NEED_TO_BUILD" = true ]; then
-  echo "========================="
-  echo ""
-  echo "> Начинаем билд"
-  echo ""
-  echo "========================="
+npm run build
+rsync -av --delete ./.output/ root@62.217.182.224:/root/landing/.output
 
-  npm run build
-fi
-
-
-# === Деплой билда ===
-echo "========================="
-echo ""
-echo "> Начинаем деплой билда"
-echo ""
-echo "========================="
-
-rsync -av --delete ./.output root@62.217.182.224:/root/landing/.output
-rsync -av --delete ./.output/public root@62.217.182.224:/var/www/landing
+npm run generate
+rsync -av --delete ./.output/public/ root@62.217.182.224:/var/www/landing
 
 
 # === Деплой кред ===
