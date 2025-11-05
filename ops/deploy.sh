13af82f552f76fdebef6de5cb90370af34fa699a
bl1nkoff <bl1nkoff.dev@gmail.com>
2025-11-05 10:44:26 +0300

    deploy

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -3,6 +3,8 @@ set -e
 
 # === Проверяем аргументы ===
 #NEED_TO_BUILD=false
+DEPLOY_GUI=false
+DEPLOY_SERVER=false
 NEED_TO_DEPLOY_CREDENTIALS=false
 
 for arg in "$@"; do
@@ -10,18 +12,31 @@ for arg in "$@"; do
 #    NEED_TO_BUILD=true
 #  fi
 
+  if [[ "$arg" == "--gui" || "$arg" == "-g" ]]; then
+    DEPLOY_GUI=true
+  fi
+
+  if [[ "$arg" == "--server" || "$arg" == "-s" ]]; then
+    DEPLOY_SERVER=true
+  fi
+
   if [[ "$arg" == "--credentials" || "$arg" == "-c" ]]; then
     NEED_TO_DEPLOY_CREDENTIALS=true
   fi
 done
 
 
-# === Билд ===
-npm run build
-rsync -av --delete ./.output/ root@62.217.182.224:/root/landing/.output
+# === Билд & Деплой приложений ===
 
-npm run generate
-rsync -av --delete ./.output/public/ root@62.217.182.224:/var/www/landing
+if [ "$DEPLOY_GUI" = true ]; then
+  npm run generate
+  rsync -av --delete ./.output/public/ root@62.217.182.224:/var/www/landing
+fi
+
+if [ "$DEPLOY_SERVER" = true ]; then
+  npm run build
+  rsync -av --delete ./.output/ root@62.217.182.224:/root/landing/.output
+fi
 
 
 # === Деплой кред ===
